034cc9921e4dcd5d093a8629da888130be55dd90
Mauro <Mauro@DESKTOP-9Q7KFUP>
2018-11-04 15:32:59 +0100

    lol

--- String/src/String.c
+++ String/src/String.c
@@ -12,23 +12,30 @@
 #include <stdlib.h>
 #include <conio.h>
 
-char* scan_string();
+typedef struct {
+	char* string;
+	int size;
+} string;
 
+void scan_string(string* s);
+string reverse(string* s);
 int main(void) {
+
+	string s1;
 	printf("Inserisci nome -> ");
-	char* string = scan_string();
-	printf("\n%s\n", string);
-	free(string);
+	//char* string = scan_string();
+	scan_string(&s1);
+	printf("\n%s\n%d", s1.string, s1.size);
+	//free(string);
 	system("pause");
 	return EXIT_SUCCESS;
 }
 
-
-char* scan_string(){
+void scan_string(string* s){
 
 	char c;
-	char* string = malloc(sizeof(char));
-
+	//char* string = malloc(sizeof(char));
+	s->string = malloc(sizeof(char));
 	int siz = 0;
 	do {
 
@@ -39,16 +46,55 @@ char* scan_string(){
 			putchar(0);
 			putchar(8);
 			siz--;
-			string = realloc(string, sizeof(char)*(siz));
+			s->string = realloc(s->string, sizeof(char)*(siz));
 		} else if ( siz >= 0 && c != 8 ){
 			putchar(c);
-			string = realloc(string, sizeof(char)*(siz+1));
-			string[siz] = c;
+			s->string = realloc(s->string, sizeof(char)*(siz+1));
+			s->string[siz] = c;
 			siz++;
 		}
 
 	} while ( c != 13 );
 
-	string[siz-1] = '\0';
-	return string;
+	putchar(10);
+	putchar(13);
+
+	s->string[siz-1] = '\0';
+	s->size = siz;
 }
+
+string reverse(string* s)
+   {
+       int i, j;
+
+       string rev;
+       rev.size = s->size;
+       rev.string = malloc(sizeof(char)*(s->size));
+
+       for (i = 0, j = rev.size-2; i<rev.size; i++, j--) {
+    	   rev.string[i] = s->string[j];
+       }
+
+       rev.string[rev.size-1] = '\0';
+       return rev;
+   }
+
+string int_to_string(int n)
+  {
+      int i;
+
+      string s;
+      s.string = malloc(sizeof(char));
+
+      i = 0;
+      do {       /* generate digits in reverse order */
+    	  s.string = realloc(s.string, sizeof(char)* (i+1));
+    	  s.string[i++] = n % 10 + '0';   /* get next digit */
+      } while ((n /= 10) > 0);     /* delete it */
+
+      s.string = realloc(s.string, sizeof(char) * (i+1));
+      s.string[i] = '\0';
+      s.size = i+1;
+
+      return reverse(&s);
+  }
